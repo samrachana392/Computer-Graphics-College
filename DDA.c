942912b6d5b26b1e5f2c533ab6a6e7993437e63d
samrachana392 <samrachana302@gmail.com>
2026-02-04 22:33:38 +0545

    changed the window size to 300,300

--- DDA.c
+++ DDA.c
@@ -54,7 +54,7 @@ int main(int argc, char **argv)
     glutInit(&argc, argv);
 
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500, 500);
+    glutInitWindowSize(300, 300);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("DDA Line Drawing Algorithm");
     

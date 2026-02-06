1f8486761ce6e8f2868a7e09d6319d95eae0ad3a
samrachana392 <samrachana302@gmail.com>
2026-02-06 14:57:50 +0545

    Updated

--- BLA.c
+++ BLA.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include <GL/Glut.h>
-
-// Assumptions we made:
-// line drawn left to right
-// slope 0 <= m <= 1
-// x_end > x_start
+#include <GL/glut.h>
 
 int x_start, y_start, x_end, y_end;
-
 
 void display()
 {
@@ -15,28 +9,60 @@ void display()
     glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_POINTS);
 
-    int dx = x_end - x_start;
-    int dy = y_end - y_start;
-
-    int p = 2 * dy - dx; // initial P0
-
     int x = x_start;
     int y = y_start;
 
-    glVertex2i(x, y); // 2i means 2 coordinates (x and y) and data type = integer
+    int dx = abs(x_end - x_start);
+    int dy = abs(y_end - y_start);
 
-    while (x < x_end) // or for (int i=0;i<dx;i++)
+    int sx = (x_end > x_start) ? 1 : -1;   // step for x
+    int sy = (y_end > y_start) ? 1 : -1;   // step for y
+
+    glVertex2i(x, y);
+
+    // if slope < 1
+    if (dx >= dy)
     {
-        if (p < 0){
-            x += 1;
-            p = p + 2 * dy;
+        int p = 2 * dy - dx;
+
+        for (int i = 0; i < dx; i++)
+        {
+            x += sx;
+
+            if (p < 0)
+            {
+                p = p + 2 * dy;
+            }
+            else
+            {
+                y += sy;
+                p = p + 2 * dy - 2 * dx;
+            }
+
+            glVertex2i(x, y);
         }
-        else{
-            x += 1;
-            y += 1;
-            p = p + 2 * dy - 2 * dx;
+    }
+    // if slope > 1
+    else
+    {
+        int p = 2 * dx - dy;
+
+        for (int i = 0; i < dy; i++)
+        {
+            y += sy;
+
+            if (p < 0)
+            {
+                p = p + 2 * dx;
+            }
+            else
+            {
+                x += sx;
+                p = p + 2 * dx - 2 * dy;
+            }
+
+            glVertex2i(x, y);
         }
-        glVertex2i(x, y);
     }
 
     glEnd();
@@ -46,26 +72,25 @@ void display()
 void init()
 {
     glClearColor(0.0, 0.0, 0.0, 1.0);
-    glMatrixMode(GL_PROJECTION);      
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0,100,0,100);
-    glPointSize(4.0f); 
+    gluOrtho2D(0, 100, 0, 100);
+    glPointSize(4.0);
 }
 
 int main(int argc, char **argv)
 {
-    printf("Enter starting points (x_start, y_start): ");
+    printf("Enter starting point (x1 y1): ");
     scanf("%d %d", &x_start, &y_start);
 
-    printf("Enter ending points (x_end, y_end): ");
+    printf("Enter ending point (x2 y2): ");
     scanf("%d %d", &x_end, &y_end);
 
     glutInit(&argc, argv);
-    
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(300, 300); // 300 height 300 widhth of window
-    glutInitWindowPosition(100, 100); // window appears 100 left of my laptop monitor/screen and 100 down of laptop monitor
-    glutCreateWindow("Bresenham Line Algorithm");
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("BLA (081BCT072 Samrachana Sharma)");
 
     init();
     glutDisplayFunc(display);
